80806c586d591b7cc30380d0db6a0529075b325c
Riadh MNASRI <riadh.mnasri@gmail.com>
2019-07-23 00:48:52 +0200

    Add EnableWebflux - optional

--- src/main/kotlin/com/wehightech/bbl/ReactiveKotlinApplication.kt
+++ src/main/kotlin/com/wehightech/bbl/ReactiveKotlinApplication.kt
@@ -2,8 +2,10 @@ package com.wehightech.bbl
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.reactive.config.EnableWebFlux
 
 @SpringBootApplication
+@EnableWebFlux
 class ReactiveKotlinApplication
 
 fun main(args: Array<String>) {
